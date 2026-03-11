6ac30b0e364dbd56c01b281fa22465ee4b5d1043
simonetini <simone.tini@uninsubria.it>
2026-03-11 16:31:16 +0100

    Added main reac.sys.

--- examples/reactionsystems/src/main/java/reacsys/Main.java
+++ examples/reactionsystems/src/main/java/reacsys/Main.java
@@ -2,8 +2,14 @@ package reacsys;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("ciao");
+
+
 
 
     }
