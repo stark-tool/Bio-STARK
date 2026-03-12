d637d737528c39832fbbe88dd6149c59e6b506a7
simonetini <simone.tini@uninsubria.it>
2026-03-12 09:53:20 +0100

    Added the skeleton of the synapsis example

--- examples/reactionsystems/src/main/java/reacsys/Main.java
+++ examples/reactionsystems/src/main/java/reacsys/Main.java
@@ -3,11 +3,31 @@ package reacsys;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
+import org.apache.commons.math3.random.RandomGenerator;
+import stark.*;
+import stark.controller.Controller;
+import stark.controller.NilController;
+import stark.ds.DataState;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("ciao");
+        try {
+
+            Controller controller = new NilController();
+
+            DataState initialState = getInitialState( );
+
+            RandomGenerator rand = new DefaultRandomGenerator();
+
+            SystemState system = new ControlledSystem(controller, (rg, ds) -> ds.apply(applyReactions(rg, ds)),initialState);
+
+
+
+        }
+        catch (RuntimeException e) {
+            e.printStackTrace();
+        }
 
 
 
